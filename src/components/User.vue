07a6dcda5c8ea8897ffda5543d05a08fed610ca4
Carrie <imr.mu@icloud.com>
2019-01-25 17:16:48 +0800

    add User.vue

---
+++ src/components/User.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1>Hello,Vue!简单的增删改查</h1>
+        
+    </div>
+</template>
+
+<script>
+// 导出常量、函数、文件、模块，导入时使用import from即可
+export default {
+    name: 'User'
+}
+</script>
